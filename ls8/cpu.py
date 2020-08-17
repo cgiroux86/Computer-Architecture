3fdc0f93c761a404e9965cc75099ee22791a2447
Chris Giroux <cgiroux86@gmail.com>
2020-08-17 17:57:19 -0400

    monday MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,10 +2,20 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.running = True
+        self.PC = 0
+        self.HLT = 0b00000001
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
+        self.NOP = 0b00000000
+
         """Construct a new CPU."""
         pass
 
@@ -18,25 +28,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +57,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -61,5 +70,28 @@ class CPU:
         print()
 
     def run(self):
-        """Run the CPU."""
-        pass
+        self.load()
+        while self.running:
+            IR = self.ram[self.PC]
+            if IR == 130:
+
+                self.reg[self.ram[self.PC + 1]] = self.ram[self.PC + 2]
+                print(self.reg[self.ram[self.PC + 1]])
+                self.PC += 3
+                print(self.ram[self.PC])
+            elif IR == 1:
+                self.running = False
+                self.PC = 0
+            elif IR == 71:
+                print(self.reg[self.ram[self.PC + 1]])
+                self.PC += 2
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+
+
+cpu = CPU()
+cpu.run()
